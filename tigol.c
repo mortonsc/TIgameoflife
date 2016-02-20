126ce21f51c5b34332022cd746c5e646bebadeb2
Scott Morton <mortonsc@umich.edu>
2016-02-19 21:54:32 -0500

    rewrite to not rely on repeated division/mod

--- tigol.c
+++ tigol.c
@@ -7,6 +7,8 @@
  * Code by Scott Morton 2016
  */
 
+#define SCREEN_WIDTH_BYTES 12
+
 void assert(int cond, const char *err)
 {
     if (!cond) {
@@ -17,85 +19,91 @@ void assert(int cond, const char *err)
 }
 
 /*
- * Sets the bit of appBackUpScreen representing the xth pixel of the yth row
+ * sets the nth bit of byte to val (leftmost bit is 0)
  */
-void set_pixel(int y, int x, bool is_on)
+void set_bit(unsigned char *byte, int n, bool val)
 {
 
-    unsigned char *byte = appBackUpScreen + (y*SCREEN_WIDTH + x/8);
-    /* ex. if x % 8 = 3, mask is 0010 0000b */
-    unsigned char mask = 0x80 >> (x % 8);
+    /* ex. if n = 3, mask is 0010 0000b */
+    int mask = 0x8000 >> n;
 
-    if (y < 0 || y >= SCREEN_HEIGHT || x < 0 || x >= SCREEN_WIDTH)
-        return;
-
-    if (is_on) {
-        /* set the (x % 8)th bit to 1, without affecting the others */
+    if (val) {
+        /* set the nth bit to 1, without affecting the others */
         *byte |= mask;
     } else {
-        /* set the (x % 8)th bit to 0, without affecting the others */
+        /* set the nth bit to 0, without affecting the others */
         *byte &= ~mask;
     }
 }
 
 /*
- * Returns the value of the bit of plotSScreen at the given position
+ *  returns the value of the nth bit of block (leftmost bit is 0)
  */
-bool get_pixel(int y, int x)
+bool get_bit(unsigned char *byte, int n)
 {
-    unsigned char *byte = plotSScreen + (y*SCREEN_WIDTH + x/8);
-    /* ex. if x % 8 = 3, mask is 0010 0000b */
-    unsigned char mask = 0x80 >> (x % 8);
-
-    /* "off-screen" pixels are always off */
-    if (y < 0 || y >= SCREEN_HEIGHT || x < 0 || x >= SCREEN_WIDTH)
-        return false;
+    /* ex. if n = 3, mask is 0010 0000b */
+    unsigned char mask = 0x80 >> n;
 
     return *byte & mask;
+}
+
+bool new_cell(int live_neighbors, bool current)
+{
+    switch(live_neighbors) {
+    case 0:
+    case 1:
+        /* death by loneliness */
+        return false;
+    case 2:
+        return current;
+    case 3:
+        /* reproduction */
+        return true;
+        break;
+    default:
+        /* death by overcrowding */
+        return false;
+        break;
+    }
 }
 
 void take_step()
 {
     /* iteration vars */
-    int y;
-    int x;
-    int i;
+    unsigned char *src_byte = plotSScreen;
+    unsigned char *start = plotSScreen;
+    unsigned char *end = plotSScreen + BUFFER_SIZE;
+    unsigned char *end_row = plotSScreen + SCREEN_WIDTH_BYTES;
+    unsigned char *dst_byte = appBackUpScreen;
+    int bit = 0;
+    unsigned char *i;
     int j;
     int live_neighbors;
 
-    /*
-     * iterate over every pixel of the screen to determine its new state
-     */
-    for (y = 0; y < SCREEN_HEIGHT; y++) {
-        for (x = 0; x < SCREEN_WIDTH; x++) {
-            live_neighbors = 0;
-            for (i = -1; i <= 1; i++)
-                for (j = -1; j <= 1; j++)
-                    if (get_pixel(y+i, x+j))
-                        live_neighbors++;
-            /*
-             * the loop above will count the current pixel as its own neighbor
-             * so we must compensate
-             */
-            if (get_pixel(y, x))
-                live_neighbors--;
-            switch(live_neighbors) {
-            case 0:
-            case 1:
-                /* death by loneliness */
-                set_pixel(y, x, false);
-                break;
-            case 2:
-                break;
-            case 3:
-                /* reproduction */
-                set_pixel(y, x, true);
-                break;
-            default:
-                /* death by overcrowding */
-                set_pixel(y, x, false);
-                break;
-            }
+    while (src_byte < end) {
+        live_neighbors = 0;
+        /*
+         * iterate over each of the current cells neighbors
+         * to count how many of them are alive
+         */
+        for (i = src_byte-SCREEN_WIDTH_BYTES;
+                i <= src_byte+SCREEN_WIDTH_BYTES; i+=SCREEN_WIDTH_BYTES)
+            for (j = bit-1; j <= bit+1; j++)
+                if (start <= i && i < end && 0 <= j &&
+                     ((j < 8 && get_bit(src_byte, j)) ||
+                      (j == 8 && src_byte < end_row && get_bit(src_byte+1, 0))))
+                    live_neighbors++;
+        /* the above loop will count a cell as its own neighbor, so we need to compensate */
+        if (get_bit(src_byte, bit))
+            live_neighbors--;
+        set_bit(dst_byte, bit, new_cell(live_neighbors, get_bit(src_byte, bit)));
+        bit++;
+        if (bit == 8) {
+            bit = 0;
+            src_byte++;
+            dst_byte++;
+            if (src_byte > end_row)
+                end_row += SCREEN_WIDTH_BYTES;
         }
     }
 
@@ -108,14 +116,20 @@ void take_step()
 int main()
 {
     memset(appBackUpScreen, 0, BUFFER_SIZE);
-    set_pixel(3, 10, true);
-    set_pixel(3, 11, true);
-    set_pixel(3, 12, true);
+    set_bit(appBackUpScreen+SCREEN_WIDTH_BYTES+4, 0, true);
+    set_bit(appBackUpScreen+SCREEN_WIDTH_BYTES+4, 1, true);
+    set_bit(appBackUpScreen+SCREEN_WIDTH_BYTES+4, 2, true);
     memcpy(plotSScreen, appBackUpScreen, BUFFER_SIZE);
     FastCopy();
 
     GetKey();
+
+    penRow = 0;
+    penCol = 0;
+    VPutS("before first step...");
     take_step();
+    penCol = 0;
+    VPutS("after first step");
     GetKey();
     take_step();
 
